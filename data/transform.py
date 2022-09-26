82448cfa05b299ace05635de83a8c119f195301d
yuanli0218 <yuanli0218@gmail.com>
2022-09-25 20:07:52 -0400

    move .py

--- data/transform.py
+++ data/transform.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 stats = pd.read_excel(
-    './data/2021/BBM_PlayerRankings.xls', 
+    './2021/BBM_PlayerRankings.xls', 
     usecols= ['Own', 'Round', 'Rank', 'Name', 'Team', 'Pos', 'USG', 
               #'g', 'm/g', 'p/g', '3/g', 'r/g', 'a/g', 's/g', 'b/g', 'fg%', 'fga/g', 'ft%', 'fta/g', 'or/g', 'to/g', 
               #'ato', 'pV', '3V', 'rV', 'aV', 'sV', 'bV', 'fg%V', 'ft%V', 'orV', 'toV', 'atoV'
