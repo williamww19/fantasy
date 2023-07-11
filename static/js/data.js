5391e4c35f4f777b59abe18e66a6f8829d8a4bbc
Wei <wwei@cppib.com>
2023-07-11 15:58:00 -0400

    update data location

--- static/js/data.js
+++ static/js/data.js
@@ -1,6 +1,6 @@
 var weekly_stats = [
-    {"Team":"Team A","GP":10,"PPTS":200,"Week":1},
-    {"Team":"Team B","GP":10,"PPTS":180,"Week":1},
-    {"Team":"Team A","GP":10,"PPTS":190,"Week":2},
-    {"Team":"Team B","GP":10,"PPTS":170,"Week":2},
+    {"Team":"Team A","GP":10,"PPTS":200,"Week":"1"},
+    {"Team":"Team B","GP":10,"PPTS":180,"Week":"1"},
+    {"Team":"Team A","GP":10,"PPTS":190,"Week":"2"},
+    {"Team":"Team B","GP":10,"PPTS":170,"Week":"2"},
 ]
