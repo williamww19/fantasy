62704beeb41fde9b17a14af4805d39f28698f62a
William Wei <wwei@cppib.com>
2023-10-26 10:13:14 -0400

    code refactor

---
+++ main.py
@@ -0,0 +1,7 @@
+from data.scripts.load_data import YfaDataLoad
+from data.scripts.transform import transform_data
+
+if __name__ == '__main__':
+    yfa_data = YfaDataLoad(league_id = 6315)
+    yfa_data.download_weekly_data()
+    transform_data()
