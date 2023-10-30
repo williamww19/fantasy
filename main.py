e42819575e640187fedd72973ef9c14ebde42450
William Wei <wwei@cppib.com>
2023-10-30 10:28:00 -0400

    add IceCold to front end

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from data.scripts.load_data import YfaDataLoad
 from data.scripts.transform import transform_data
 
 if __name__ == '__main__':
-    yfa_data = YfaDataLoad(league_id=6315)
+    yfa_data = YfaDataLoad(league_id=33128)
     yfa_data.download_weekly_data()
     transform_data()
