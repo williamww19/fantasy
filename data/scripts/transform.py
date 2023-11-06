94b908ed14f6d26c97990d993f888890cefd1ff9
William Wei <wwei@cppib.com>
2023-11-06 08:34:45 -0500

    Week02

--- data/scripts/transform.py
+++ data/scripts/transform.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 
-def transform_data(league_id) -> None:
+def transform_data_to_json(league_id) -> None:
     print(f'---- transforming data to json for {league_id} ----')
     # Loop through each file in the directory
     file_dir = f'./data/league_{league_id}/weekly_stats'
@@ -28,13 +28,15 @@ def create_weekly_summary(league_id):
     file_dir = f'./data/league_{league_id}/weekly_stats'
     for file in os.listdir(file_dir):
         df_week = pd.read_csv(os.path.join(file_dir, file), encoding='utf-8')
-        week = df_week['Week'].unique()[0]
+        df_week.replace('/', 0, inplace=True)
         df_week['TO'] = -df_week['TO']
         df_week_summary = pd.merge(df_week['Team'], df_week['Team'], how='cross')
         df_week_summary = df_week_summary[df_week_summary['Team_x'] != df_week_summary['Team_y']]
         df_week_summary['results'] = df_week_summary.apply(
             lambda row: compare_team_stats(df_week, row['Team_x'], row['Team_y']), axis=1)
         df_week_summary_pivot = df_week_summary.pivot(index='Team_x', columns='Team_y', values='results')
+        df_week_summary_pivot.index.name = None
+        df_week_summary_pivot.columns.name = None
         df_week_summary_pivot.to_csv(f'./data/league_{league_id}/weekly_summary/{file}')
 
 
@@ -47,3 +49,9 @@ def compare_team_stats(df_week: pd.DataFrame, team_a: str, team_b: str) -> str:
     loss = ((stats_comp['Team_x'] - stats_comp['Team_y']) < 0).sum()
 
     return f'{win}-{loss}-{tie}'
+
+
+if __name__ == '__main__':
+    league_id = 6315
+    transform_data_to_json(league_id=league_id)
+    create_weekly_summary(league_id=league_id)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from data.scripts.load_data import YfaDataLoad
-from data.scripts.transform import transform_data, create_weekly_summary
+from data.scripts.transform import transform_data_to_json, create_weekly_summary
 
 if __name__ == '__main__':
     for league in [6315, 33128]:
         yfa_data = YfaDataLoad(league_id=league)
         yfa_data.download_weekly_data()
-        transform_data(league_id=league)
+        transform_data_to_json(league_id=league)
         create_weekly_summary(league_id=league)
