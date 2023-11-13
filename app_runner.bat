649beab25169f3f07e20e9dc36c71c1193aa321e
williamww19 <wyl11wyl@gmail.com>
2023-11-12 23:28:21 -0500

    Automated commit - 2023-11-12 23:28:21

--- app_runner.bat
+++ app_runner.bat
@@ -11,8 +11,18 @@ C:\Users\wylyy\Documents\Personal\Fantasy\webapp\venv\Scripts\python.exe C:\User
 rem Add changes to Git
 git add .
 
-rem Commit changes
-git commit -m "Automated commit"
+rem Get current date and time
+for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set datetime=%%a
+set datetime=%datetime:~0,14%
+set year=%datetime:~0,4%
+set month=%datetime:~4,2%
+set day=%datetime:~6,2%
+set hour=%datetime:~8,2%
+set minute=%datetime:~10,2%
+set second=%datetime:~12,2%
+
+rem Commit changes with timestamp
+git commit -m "Automated commit - %year%-%month%-%day% %hour%:%minute%:%second%"
 
 rem Push changes
 git push
