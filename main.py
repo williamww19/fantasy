7c90341fed59984b8d68d609d3c76e10980bd524
William Wei <wwei@cppib.com>
2024-10-25 10:27:09 -0400

    Week01-Day2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from data.scripts.load_data import YfaDataLoad
 from data.scripts.transform import transform_data_to_json, create_weekly_summary
 
 if __name__ == '__main__':
-    for league in [13648, 33023]:
+    for league in [13648, 33203]:
         yfa_data = YfaDataLoad(league_id=league)
         yfa_data.download_weekly_data()
         transform_data_to_json(league_id=league)
